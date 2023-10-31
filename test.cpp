fc04e1673df4060c82cc5418d73116855ed73987
siwangqishiq <525647740@qq.com>
2023-10-31 17:35:48 +0800

    update

---
+++ test.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+int main(){
+    Display *display = XOpenDisplay(nullptr);
+    if(display == nullptr){
+        std::cerr << "Can not open display" <<std::endl;
+        return -1;
+    }
+
+    int screen = DefaultScreen(display);
+
+    Window window = XCreateSimpleWindow(display,
+        RootWindow(display , screen),
+        0,0,400,300,1 , 
+        BlackPixel(display,screen),
+        WhitePixel(display,screen));
+    
+    std::cout << "create window success" << std::endl;
+
+    Atom wmDeleteWindow = XInternAtom(display, "WM_DELETE_WINDOW", False);
+    XSetWMProtocols(display , window , &wmDeleteWindow , 1);
+    
+    XSelectInput(display , window , 
+        ExposureMask 
+        | KeyPressMask 
+        | LeaveWindowMask 
+        | PointerMotionMask 
+        | PointerMotionHintMask
+        | ClientMessage
+        | StructureNotifyMask);
+
+    XMapWindow(display , window);
+
+    XStoreName(display, window , "Hello XLIB");
+
+    std::cout << "create XStoreName success" << std::endl;
+
+    XFontStruct *fontInfo = XLoadQueryFont(display , "6x13");
+    if(fontInfo == nullptr){
+        std::cerr << "fontInfo is null quit!" <<std::endl;
+        return -2;
+    }
+    GC gc = XCreateGC(display , window , 0 , nullptr);
+
+    XSetFont(display , gc , fontInfo->fid);
+
+    XSetForeground(display , gc , WhitePixel(display , screen));
+
+    XEvent event;
+
+    while (true) {
+        XNextEvent(display, &event);
+
+        // std::cout << "event type:" << event.type << std::endl;
+        if (event.type == Expose) {
+            XDrawString(display, window, gc, 10, 30, "Hello window", 12);
+        }
+        if (event.type == KeyPress || event.type == ClientMessage) {
+            break;
+        }
+
+    }//end while
+
+    XUnloadFont(display, fontInfo->fid);
+    XFreeGC(display, gc);
+    XDestroyWindow(display, window);
+    XCloseDisplay(display);
+
+    return 0;
+}
+
